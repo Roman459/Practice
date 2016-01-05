b22313e987f1298018c8a654f2209a8902e9cd37
Roman459 <tagirov@360@mail.ru>
2016-01-05 13:28:09 +0600

    Database users with angular.js
    Database users with angular.js

---
+++ task #3/MvcApplication3/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication3.Models;
+
+namespace MvcApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(HttpPostedFileBase file)
+        {
+           
+                if (ModelState.IsValid)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/ImagesUsers"), fileName);
+                    file.SaveAs(path);
+                }
+          
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/ImagesUsers"), fileName);
+                file.SaveAs(path);
+            }
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ task #3/MvcApplication3/Controllers/UsersController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using MvcApplication3.Models;
+using System.Web;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace MvcApplication3.Controllers
+{
+    public class UsersController : ApiController
+    {
+
+        UserContext db = new UserContext();
+
+        public IEnumerable<User> GetUsers()
+        {
+            return db.Users;
+        }
+
+        public User GetUser(int id)
+        {
+            User user = db.Users.Find(id);
+            return user;
+        }
+
+        [HttpPost]
+        public void CreateUser([FromBody]User user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(String.Empty, ex);
+            }
+        }
+
+        [HttpPut]
+        public void EditUser(int id, [FromBody]User user)
+        {
+            if (id == user.Id)
+            {
+                db.Entry(user).State = EntityState.Modified;
+
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user != null)
+            {
+                db.Users.Remove(user);
+                db.SaveChanges();
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
